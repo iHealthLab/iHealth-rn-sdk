22b6f89c1ec584c453e07aaad4378bb77a06f713
Alan Oakden <admin@alanoakden.com>
2023-11-29 19:32:56 +0000

    Add BP5S integration, update readme, patch nordicSemi package version

---
+++ example/src/api/getAPI.js
@@ -0,0 +1,52 @@
+import bt550API from './550btAPI'
+import bg1aAPI  from './bg1aAPI'
+import am6API   from './am6API'
+import bg5sAPI   from './bg5sAPI'
+import bp5sAPI from './bp5sAPI'
+import { 
+  BG1AModule,
+  BP550BTModule,
+  AM6Module,
+  BG5SModule,
+  BP5SModule
+ } from '@ihealth/ihealthlibrary-react-native'
+
+export default {
+
+  getDeviceNotify: (type) => {
+    switch(type) {
+      case 'KN550':
+        return BP550BTModule.Event_Notify
+
+      case 'BG1A':
+        return BG1AModule.Event_Notify
+
+      case 'AM6':
+        return AM6Module.Event_Notify
+      case 'BG5S':
+        return BG5SModule.Event_Notify
+      case 'BP5S':
+        return BP5SModule.Event_Notify
+    }
+  },
+
+  getDeviceAPI: (type) => {
+    switch(type) {
+      case 'KN550':
+        return bt550API
+
+      case 'BG1A':
+        return bg1aAPI
+
+      case 'AM6':
+        return am6API
+      
+      case 'BG5S':
+        return bg5sAPI
+
+      case 'BP5S':
+        return bp5sAPI
+            
+    }
+  }
+}
